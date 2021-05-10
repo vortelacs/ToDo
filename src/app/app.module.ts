3fc8acbf8d616129a692081ac1911331a9c4e5a0
vortelacs <62349187+vortelacs@users.noreply.github.com>
2021-05-11 01:46:16 +0300

    Added categories component and service(static), performed template decomposition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoriesComponent } from './views/categories/categories.component';
+import { DataHandlerService } from './service/data-handler.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
